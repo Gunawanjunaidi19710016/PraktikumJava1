32d26a0cf1b3c8d16a60ebd76e80eb7d7757b21e
Gunawan <gunawanjndreal06@gmail.com>
2021-06-04 19:53:45 +0700

    output dengan sout

---
+++ Modul01/output.java
@@ -0,0 +1,22 @@
+public class output{
+ public static void main(String[] args){
+
+   String namaDepan = "Gunawan";
+   String namaTengah = "Junaidi";
+   String namaBelakang = "Real";
+   int usia = 19;
+   int targetTahunKuliah = 4;
+   double ipk = 3.85;
+   char nilaiAbjad = 'A';
+   boolean tampan = true;
+
+   System.out.println("nama depan : " + namaDepan);
+   System.out.println("nama tengah : " + namaTengah);
+   System.out.println("nama belakang : " + namaBelakang);
+   System.out.println("Usia : " + usia);
+   System.out.println("Target Kuliah : " + targetTahunKuliah + "tahun");
+   System.out.println("Ipk : " + ipk);
+   System.out.println("Nilai PBO : " + nilaiAbjad);
+   System.err.println("Tampan  : " + tampan);
+    }
+  }
