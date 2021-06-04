2f7aa1baaa763875554ac161c749f676bc0d817d
Gunawanjunaidi19710016 <85158801+Gunawanjunaidi19710016@users.noreply.github.com>
2021-06-04 21:12:12 +0800

    Message

---
+++ Modul01/MessageDialog.java
@@ -0,0 +1,25 @@
+import javax.swing.JOptionPane;
+public class MessageDialog{
+ public static void main(String[] args){
+
+   String namaDepan = "Gunawan";
+   String namaTengah = "Junaidi";
+   String namaBelakang = "Real";
+   int usia = 19;
+   int targetTahunKuliah = 4;
+   double ipk = 3.85;
+   char nilaiAbjad = 'A';
+   boolean tampan = true;
+
+   System.out.println("nama depan : " + namaDepan);
+   System.out.println("nama tengah : " + namaTengah);
+   System.out.println("nama belakang : " + namaBelakang);
+   System.out.println("Usia : " + usia);
+   System.out.println("Target Kuliah : " + targetTahunKuliah + "tahun");
+   System.out.println("Ipk : " + ipk);
+   System.out.println("Nilai PBO : " + nilaiAbjad);
+   System.err.println("Tampan  : " + tampan);
+
+   JOptionPane.showMessageDialog(null,"Hai," + namaDepan + namaTengah + namaBelakang);
+    }
+  }
